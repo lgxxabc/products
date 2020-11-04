a8dcd41b62d1955154ab4e1c0953ad1c05ebed77
Fox Liu <lgxxabc@gmail.com>
2020-11-04 09:37:59 -0600

    add check file exist

--- products.py
+++ products.py
@@ -1,33 +1,40 @@
 #######################################################
-# Class 65
-# Read from file 'products.csv'
-# Steps:
-# 1. .split('.')，用comma來做分割
-# 2. .strip()來除掉換行符號(\n)
-# 3. 加上encoding = 'utf-8'來去除UnicodeDecodeError
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		# get rid of the header when reading the file
-		if 'product, price' in line:
-			continue
+# Check if the file exists, before reading it.
+import os		# operating system
 
-		# s = line.strip().split(',')
-		# name = s[0]
-		# price = s[1]
-		# this is equal to:
-		name, price = line.strip().split(',')
-		# 把讀到的内容裝進list
-		products.append([name, price])
-print(products)
-# Outputs:
-# products: 讓user重複輸入他買過的products
-# while loop vs. for loop
-# when we are not sure how many loops are there, we choose while loop
+products = []
+
+if os.path.isfile('products.csv'):
+	print('File exists.')
+	
+	# Read from file 'products.csv'
+	# Steps:
+	# 1. .split('.')，用comma來做分割
+	# 2. .strip()來除掉換行符號(\n)
+	# 3. 加上encoding = 'utf-8'來去除UnicodeDecodeError
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			# get rid of the header when reading the file
+			if 'product, price' in line:
+				continue
+
+			# s = line.strip().split(',')
+			# name = s[0]
+			# price = s[1]
+			# this is equal to:
+			name, price = line.strip().split(',')
+			# 把讀到的内容裝進list
+			products.append([name, price])
+	print(products)
+	# Outputs:
+	# products: 讓user重複輸入他買過的products
+	# while loop vs. for loop
+	# when we are not sure how many loops are there, we choose while loop
+
+else:
+	print('Could not find the file.')
 
 ############################################################
-# Class 64
-products = []
 # 請使用者輸入
 while True:
 	name = input('Please enter products you purchased: ')
@@ -50,6 +57,7 @@ print(products)
 # # ['b', '2']
 # # ['c', '3']
 
+##################################################
 # 印出所有商品
 for product in products:
 	print(product[0])
@@ -61,6 +69,7 @@ for product in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
+####################################################
 # Write the outputs in a file（寫入檔案）
 with open('products.csv', 'w') as f:
 # with open('products.csv', 'w', encoding = 'utf-8') as f:
