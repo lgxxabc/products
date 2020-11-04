c3bbeba3c52435ff217807ab169a244246e416bd
Fox Liu <lgxxabc@gmail.com>
2020-11-04 09:21:05 -0600

    add read file function

--- products.py
+++ products.py
@@ -1,7 +1,34 @@
+#######################################################
+# Class 65
+# Read from file 'products.csv'
+# Steps:
+# 1. .split('.')，用comma來做分割
+# 2. .strip()來除掉換行符號(\n)
+# 3. 加上encoding = 'utf-8'來去除UnicodeDecodeError
+products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		# get rid of the header when reading the file
+		if 'product, price' in line:
+			continue
+
+		# s = line.strip().split(',')
+		# name = s[0]
+		# price = s[1]
+		# this is equal to:
+		name, price = line.strip().split(',')
+		# 把讀到的内容裝進list
+		products.append([name, price])
+print(products)
+# Outputs:
 # products: 讓user重複輸入他買過的products
 # while loop vs. for loop
 # when we are not sure how many loops are there, we choose while loop
+
+############################################################
+# Class 64
 products = []
+# 請使用者輸入
 while True:
 	name = input('Please enter products you purchased: ')
 	if name == 'q':
@@ -23,17 +50,18 @@ print(products)
 # # ['b', '2']
 # # ['c', '3']
 
-# for product in products:
-# 	print(product[0])
-# # a
-# # b
-# # c
+# 印出所有商品
+for product in products:
+	print(product[0])
+# a
+# b
+# c
 
 # # String calculation
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
-# Write the outputs in a file 
+# Write the outputs in a file（寫入檔案）
 with open('products.csv', 'w') as f:
 # with open('products.csv', 'w', encoding = 'utf-8') as f:
 # if we want to print Chinese characters, then need to add encoding
